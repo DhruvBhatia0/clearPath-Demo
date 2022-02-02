3499d8e8f2d0cb7b9f1a13e09c50a73a09843e04
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2022-02-02 12:24:09 -0500

    imporved formatting

--- clearpath/components/drop-menu.js
+++ clearpath/components/drop-menu.js
@@ -2,18 +2,17 @@ import styles from '../styles/Home.module.css'
 
 function generateoptions(option){
     return (
-        <option value = {option}>{option}</option>
+        <option value = {option} key = {option}>{option}</option>
     )
 } 
 export default function menu(props) {
     return (
-        <div className="menu">
+        <div className="menu" key = {props.id}>
             <label>{props.label} : </label>
             <select name = {props.label} id = {props.label}>
                 {props.options.map(generateoptions)}
             </select>
             <br></br>
-            <hr></hr>
         </div>
     );
   }
